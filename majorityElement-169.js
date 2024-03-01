70079a007bed1902e55e3bccbeb5ae24ecf52f06
Hussain Ali <huali235@gmail.com>
2024-03-01 10:49:19 -0500

    solved problem

--- majorityElement-169.js
+++ majorityElement-169.js
@@ -4,8 +4,29 @@
 */
 
 function majorityElement(nums) {
-    let hashmap = Sort([nums])
-    return hashmap
+    let hashmap = new Map();
+
+    for(let i = 0; i < nums.length; i++){
+        if(hashmap.has(String(nums[i]))){
+            let currentValue = hashmap.get(String(nums[i]));
+            hashmap.set(String(nums[i]), currentValue + 1);
+        } else {
+            hashmap.set(String(nums[i]), 1);
+        }
+    }
+
+    let maxKey = null;
+    let maxValue = 0;
+
+    for([key, value] of hashmap){
+        if(value > maxValue){
+            maxValue = value;
+            maxKey = key;
+        }
+    }
+
+    return Number(maxKey);
 }
 
-console.log(majorityElement([3,2,3,2,2,2]))
+console.log(majorityElement([3,2,3]))
+console.log(majorityElement([2,2,1,1,1,2,2]))
